6d7e17fc1bcee552eee414c2c794151b68e1e486
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-16 04:01:51 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - Aprogramm that prints a line with puts function
- * Return: 0 (Sucess)
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

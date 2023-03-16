c60005fbf93b09bcf8d2187756b364bdddd8c4ab
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-16 04:58:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19,\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	return (1);
 }
 

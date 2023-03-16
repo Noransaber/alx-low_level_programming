6ccd5b54ddf65462718997327763be618f85467f
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-16 03:55:38 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
+/*
+ * main - Aprogramm that prints a line with puts function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
 puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
+

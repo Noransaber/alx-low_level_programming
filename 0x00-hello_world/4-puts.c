d43d23a3118f639ed562b86f6a0684ee8d7f519c
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-16 03:50:16 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-int main() {
-int char MyStr[] = "Programming is like building a multilingual puzzle\n";
-puts(MyStr);
-return0;
+int main(void) {
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }

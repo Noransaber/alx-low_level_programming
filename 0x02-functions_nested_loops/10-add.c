9935128f981eafecb05d9c389dd09cc726f94005
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 06:26:10 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - that adds two integers and returns the result.
+ * @a: firrst number
+ * @b: the sec number
+ * Return: the result
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+

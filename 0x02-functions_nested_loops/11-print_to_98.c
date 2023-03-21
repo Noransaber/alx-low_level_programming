597af827c936cbcf2d6da3c4aae9774c3b62c307
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 08:22:48 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,30 +13,26 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (n == 98)
-			{
-				printf("%d, ", i);
-			}
-			printf('\n');
+			printf("%d, ", i);
 		}
+		printf("\n");
 	}
-			else if (n < 0)
-			{
-				for (i = n; i <= 98; i++)
-				{
-					printf("%d, ", i);
-				}
-				printf('\n');
-		}
-	}
-	else if (n > 98)
+	else if (n < 0)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
 		}
-		printf('\n');
+		printf("\n");
 	}
+			else if (n > 98)
+			{
+				for (i = n ; i > 98 ; i--)
+				{
+					printf("%d, ", i);
+				}
+				printf("\n");
+			}
 	else if (n == 98)
 	{
 		printf("%d, ", n);

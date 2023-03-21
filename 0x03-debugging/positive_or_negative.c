fd4738472eb9dd45fe0734dd22725954e8d41424
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 10:54:26 -0700

    3-print_remaining_days.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative - check if the num is pos pr nega
+ * @i: the number to be checked
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+
+		printf("%d is negative\n", i);
+		else
+			printf("%d is positive\n", i);
+	}

e98ec495efda5d5b9ab3748f49d04ab67e40c17c
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 03:08:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#includ "main.h"
+
+/**
+ * print_alphabet - print all alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}

01ea015f063d567e8ba79734117d1b788e347225
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 08:08:12 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0 &&n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -19,6 +19,7 @@ void print_to_98(int n)
 			}
 			printf('\n');
 		}
+	}
 			else if (n < 0)
 			{
 				for (i = n; i <= 98; i++)

9ea0a19509b50214ab408bc1808e67f49b93d8f9
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 03:34:48 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	char letter;
 
 	while (num <= 10)
-		num++;
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
+		num++;
 	}
 }

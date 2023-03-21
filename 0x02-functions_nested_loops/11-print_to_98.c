a11abff5130e19e5fab992d89fa6e3081392f6be
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-21 06:33:50 -0700

    print tp 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: is the number
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		printf('%d, ', n);
+	}
+}

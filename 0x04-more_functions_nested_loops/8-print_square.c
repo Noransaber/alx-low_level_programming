e7b359cb089290e623f8fbde4fdea2ac4251a403
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 15:04:31 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square -  that prints a square
+ * @size: number
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int x, y;
+	x = 0;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (;x < size; x++)
+	{
+	for (y = 0; y <= size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}

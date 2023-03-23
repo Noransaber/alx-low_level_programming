61628331d6c1d72e9534aea3aff9410404cecb15
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 11:42:11 -0700

    update in 4-print_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,16 @@ void print_most_numbers(void)
 {
   int i;
 
-  for (i = 0; i <= 9; i++) {
-    if (i == 2 || i == 4)
-    {
-      continue;
-
-    } 
-    else
-    {
-      _putchar(i);
-    }
+  for (i = 0; i <= 9; i++)
+  {
+  if (i == 2 || i == 4)
+  {
+  continue;
+  } 
+  else
+  {
+  _putchar(i);
   }
+  }
+  _putchar('\n');
 }

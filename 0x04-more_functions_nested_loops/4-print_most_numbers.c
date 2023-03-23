bac22a57bee9604e489aa0be051433e37f85c3ab
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 08:54:59 -0700

    print number withou 2 pr 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+}

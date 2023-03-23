3b67e1208388010dbc514e72c8d135d722baba3f
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 15:59:36 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-	if (x % 3 == 0)
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (x % 3 == 0)
 	{
 	printf("Fizz");
 	}
 	else if (x % 5 == 0)
 	{
 	printf("Buzz");
-	}
-	else if ((x % 3 == 0) && (x % 5 == 0))
-	{
-	printf("FizzBuzz");
 	}
 	else
 	{

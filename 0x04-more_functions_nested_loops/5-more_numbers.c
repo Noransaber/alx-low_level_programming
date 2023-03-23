8b86f3ce8820f63c8c8eada064554378cfdae99f
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 11:53:39 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int t;
+	int i;
+
+	t = 0;
+
+	while (t <= 10)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+	_putchar(i + '0');
+	}
+	t++
+	}
+}

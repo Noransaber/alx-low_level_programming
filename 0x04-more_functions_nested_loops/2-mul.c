dc767ac234c261459b8377b119e91121458e410e
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-23 08:03:11 -0700

    multiplicarion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers
+ * @a: number to be multiplied
+ * @b: number to be multplied
+ * Return: multiplication of two nums
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

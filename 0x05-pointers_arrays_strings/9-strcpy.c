a1cd30981e2ebfedfffcfb02eb34cf8adb076f5a
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-27 15:22:52 -0700

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,19 +11,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int n;
-	
-	while (src[n] != '\0')
-	{
-	n++;
-	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
 	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
-	return (0);
+	dest[i++] = '\0';
+	return (dest);
 }

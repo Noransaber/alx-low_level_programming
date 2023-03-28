b5d3b2c01b2ba0237d8a7641a18d61e2c6d7c034
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-27 19:11:34 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,22 @@
 
 /**
  * print_array - prints n elements of an array of integers
- *@a: array
- @n: numbers
-Return: void
-/
+ * @a: array
+ * @n: number
+ * Return: void
+ */
 
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	if (i != n - 1)
+	{
+	printf(", ");
+	}
+	}
+	printf("\n");
+}

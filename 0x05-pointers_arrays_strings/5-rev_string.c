59b5be34a32220beade2f23462985aa61d39ee52
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-27 20:29:14 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,17 @@
 
 void rev_string(char *s)
 {
-	int len = 0;
-	int index;
-	char tmp;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	while (s[index++])
-	len++;
-
-	for (index = len -1; index >= len / 2; index--)
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-	tmp = s[index];
-	s[index] = s[len - index - 1];
-	s[len - index - 1] = tmp;
+	counter--;
+	rev = s[i];
+	s[i] = s[counter];
+	s[counter] = rev;
 	}
 }

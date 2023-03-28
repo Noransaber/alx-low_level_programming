9b3b46432e5f98c950c33e6e91ed840451a9f767
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-27 20:36:56 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,23 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
+	int t = 0;
+	int o;
+	char *y =str;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (*y != '\0')
 	{
-	_putchar(str[i]);
+	y++;
+	i++;
+	}
+	t = i - 1;
+	for (o = 0; o <= t;  o++)
+	{
+	if (o % 2 == 0)
+	{
+	_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
 }

b683127de98298c520bec49ecbff14031919a6f8
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-29 13:37:14 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rot13 -  encodes a string
+ * @:
+ * Return: the value
+ */
+
+char *rot13(char *)
+{
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,4 +21,14 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+void print_number(int n);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+
+
 #endif

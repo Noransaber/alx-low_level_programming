c3c55d80f216f19d1270dbed4f4039329495d06b
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-29 13:14:06 -0700

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }

3f8d101cdf2e5b36f0830b26d35d06cd2005e157
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-29 14:31:34 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,6 +6,21 @@
  * Return: the value
  */
 
-char *rot13(char *)
+char *rot13(char *str)
 {
+	char *output;
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+	output[i] = (char)(((str[i] - 13) % 26) + 'a');
+	}
+	if (str[i] >= 'A' && str[i] <= 'Z')
+	{
+	output[i] = (char)(((str[i] - 13) % 26) + 'A');
+	}
+	}
+	return (output);
+}

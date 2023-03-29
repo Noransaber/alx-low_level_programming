4bb4e905f2bf03650812a8cce30267933d90457d
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-29 14:36:39 -0700

    edit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,19 +8,20 @@
 
 char *rot13(char *str)
 {
-	char *output;
-	int i;
+	int i, j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (str[i] >= 'a' && str[i] <= 'z')
-	{
-	output[i] = (char)(((str[i] - 13) % 26) + 'a');
+		for (j = 0; j < 52; j++)
+		{
+		if (str[i] == data1[i])
+		{
+		str[i] = data2[i];
+		break;
+		}
+		}
+		return(str);
 	}
-	if (str[i] >= 'A' && str[i] <= 'Z')
-	{
-	output[i] = (char)(((str[i] - 13) % 26) + 'A');
-	}
-	}
-	return (output);
 }

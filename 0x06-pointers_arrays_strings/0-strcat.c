e84cb6dc5c62f0789232e93be8f5e1addafb25aa
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-29 10:33:08 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: returned value
+ * @src: source of the str
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}

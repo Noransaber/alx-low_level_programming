4d1985069ec6ac5b2e019e2bf534145c47d59186
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-03 10:31:40 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes.
+ * @s:stirng
+ * @accept:another string
+ * Return:dest
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	char *dest = strpbrk(s, accept);
+	return (dest);
+}

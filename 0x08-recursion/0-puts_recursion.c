a0bbeed7ad2ccc2b30f245345806699468f17a2d
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-05 10:47:16 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
- #include <stdio.h>
 
 /**
  * _puts_recursion - prints a string
@@ -9,7 +8,8 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') {
+	if (*s == '\0')
+	{
 	putchar('\n');
 	return;
 	}

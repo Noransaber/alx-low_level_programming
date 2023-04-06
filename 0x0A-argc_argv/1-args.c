67366c020277712a635f892054ce080d01f017d3
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-06 12:45:26 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints num of args
+ * @agrc: count the num of args
+ * @argv: array of arfs
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}

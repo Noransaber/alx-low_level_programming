1b91eb556154029fd4f422e4e89fe9f452cd182b
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-11 12:26:35 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,21 +13,28 @@ int **alloc_grid(int width, int height)
 	int **grid;
 	int i, j;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 	{
 	return (NULL);
 	}
 	grid = (int **)malloc(height * sizeof(int));
-	for (i = 0; i < height; i++)
-	{
-	grid[i] = (int *)malloc(width * sizeof(int));
-	}
 	if (grid == NULL)
 	{
 	return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
+	grid[i] = (int *)malloc(width * sizeof(int));
+	if (grid[i] == NULL)
+	{
+	for (j = 0; j < i; j++)
+	{
+	free(grid[j]);
+	}
+	free(grid);
+	return (NULL);
+	}
+	/* Initialize all elements of the 2D array to 0*/
 	for (j = 0; j < width; j++)
 	{
 	grid[i][j] = 0;

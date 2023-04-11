830953f8cbd3af8798d37ab898bfa3e89e1279d6
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-11 15:12:09 -0700

    update

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * free_grid - frees a 2 dimensional grid
@@ -8,4 +9,11 @@
 
 void free_grid(int **grid, int height)
 {
+	int i;
 
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+	free(grid);
+}

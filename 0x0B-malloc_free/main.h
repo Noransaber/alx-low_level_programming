636025f1cff9296f6c07e81a6e21aee87e044b50
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-10 21:14:04 -0700

    basic preparations

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+int _putchar(char);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+#endif

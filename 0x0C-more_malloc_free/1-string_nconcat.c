9f634f1c6cf50dbcba199d4591d317a2a3379dfc
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-13 13:55:04 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,43 +12,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2, total_len;
-	char *new_arr;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *s;
 
-	if (s1 == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-	s1 = "";
+		s[i] = s1[i];
+		i++;
 	}
-	if (s2 == NULL)
-	{
-	s2 = "";
-	}
-	while (s1[len1] != '\0')
-	{
-	len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-	len2++;
-	}
-	if (n >= len2)
-	{
-	n = len2;
-	}
-	total_len = len1 + n + 1;
-	new_arr = (char *)malloc(sizeof(char) * (len1 + n + 1));
-	if (new_arr == NULL)
-	{
-	return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-	new_arr[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-	new_arr[len1 + j] = s2[j];
-	}
-	new_arr[total_len] = '\0';
-	return (new_arr);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+

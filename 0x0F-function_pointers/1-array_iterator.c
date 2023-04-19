5d9796af7d674edc261aa460f3e330b398bffe88
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-19 10:47:02 -0700

    fourth update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,24 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * array_iterator - print elem of arr
- * @size: sizeof arr
- * @array: array
- * @action: pointer tp print
- * Return: void
- */
-
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
+* array_iterator - prints each  ele of an arr
+* @array: arr
+* @size: elem in aara
+* @action: pointer to print in regular
+* Return: void
+*/
+	void array_iterator(int *array, size_t size, void (*action)(int))
 	{
-	action(array[i]);
+		unsigned int i;
+
+
+		if (array == NULL || action == NULL)
+			return;
+
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
-}
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
 #include <stddef.h>
 #include <stdlib.h>
-#ifndef main_h
-#define main_h
 
-int _putchar(char);
 
 void print_name(char *name, void (*f)(char *));
-
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
+
 #endif
+

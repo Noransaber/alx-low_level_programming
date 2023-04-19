55eb0a1da70ad0b4c3a0ce8aa57c56aca5f4f74c
Noran S Abdelfattah <noransaber685@gmail.com>
2023-04-19 10:21:57 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,12 @@
 #ifndef PRINT_NAME_C
 #define PRINT_NAME_C
 
+/**
+ * print_name - prints the name
+ * @name: pointer no the name
+ * @f: pointer func
+ * Return: voide
+ */
 void print_name(char *name, void (*f)(char *))
 {
 f(name);

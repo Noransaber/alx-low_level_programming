3e2efdad3be182f021b6997636050564914e1729
Noransaber <noransaber685@gmail.com>
2023-04-26 23:07:37 -0700

    preparing the files

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,12 @@
+
+0x12. C - Singly linked lists
+
+0. Print list
+
+1. List length
+
+2. Add node
+
+3. Add node at the end
+
+4. Free list
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+int _putchar(char);
+
+size_t print_list(const list_t *h);
+
+ize_t list_len(const list_t *h);
+
+list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
+
+#endif

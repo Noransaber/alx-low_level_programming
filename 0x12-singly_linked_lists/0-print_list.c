55bc6ab2f448b58e281b60fccdef4d8c972c2d49
Noransaber <noransaber685@gmail.com>
2023-04-27 02:22:57 -0700

    removing the space at the begining of the line for betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
- size_t count = 0;
+size_t count = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- count++;
- }
-
- return (count);
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
 }
 

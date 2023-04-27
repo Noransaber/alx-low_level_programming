97a199ab8b43231ca041c5dffb5c12787f79fb48
Noransaber <noransaber685@gmail.com>
2023-04-27 08:16:32 -0700

    source code for free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,41 +1,20 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a linked list
- * @head: double pointer to the list_t list
- * @str: string to put in the new node
- *
- * Return: address of the new element, or NULL if it failed
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-list_t *new; /* Declare a ptr to the new node*/
-list_t *temp = *head; /* Declare temp ptr to trav list init to h of list*/
-unsigned int len = 0; /* Declare int to hold len of str,init it to 0*/
+list_t *temp; /*Declare a pointer to a temporary node*/
 
-while (str[len])
-len++;
-
-new = malloc(sizeof(list_t)); /* Allocate memo for the new node*/
-if (!new) /* Check if malloc failed*/
-return (NULL); /* NULL to indicate failure*/
-
-new->str = strdup(str); /* dupl the str, assign it to new node's str*/
-new->len = len; /* Assign the len of input str to new node's len*/
-new->next = NULL;
-
-if (*head == NULL) /* If it emp,set h to point to new node,return a ptr to it*/
+while (head)
 {
-*head = new;
-return (new);
+temp = head->next; /*Set the temporary pointer to the next node in the list*/
+free(head->str);/*Free memo allocated for str field of current node*/
+free(head);/*Free memo allocated for current node*/
+head = temp;/*Set h ptr to point to next node in list*/
+}
 }
 
-while (temp->next)
-temp = temp->next;
-
-temp->next = new; /* Set next fi of last node in list to point to new node*/
-
-return (new); /* ptr to new node*/
-}

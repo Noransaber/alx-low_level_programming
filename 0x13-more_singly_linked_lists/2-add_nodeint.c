8d89b50b2e8772258dc688e64afc62589ac66c24
Noransaber <noransaber685@gmail.com>
2023-04-27 23:42:12 -0700

    source code of add_nodeint

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to the head of the list
+ * @n: new int val
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n){
+
+listint_t  *New_Start_Node = NULL; /*declare a new pointer to the list*/
+New_Start_Node = (struct listint_s *)malloc(sizeof(struct listint_s)); /*allocate memo for the new pointer*/
+if (New_Start_Node == NULL){ /*check if the allocation is failed */
+return (NULL);/*if so Return NULL*/
+}
+
+
+New_Start_Node->n = n; /*update the value of the data by new one*/
+New_Start_Node->next = *head;/*update the value of add by the head address*/
+*head = New_Start_Node; /*update the head to point to the new node*/
+return (New_Start_Node);
+
+
+
+}

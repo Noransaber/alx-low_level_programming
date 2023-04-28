a90e176aa0eeec69d79fe3e4d42005e4bea6bba0
Noransaber <noransaber685@gmail.com>
2023-04-27 20:12:11 -0700

    adding describiton and the code to make it run before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
-#include "lists.h"
 #include <stdio.h>
 
-void beforeMain()
+void beforeMain(void)__attribute__((constructor));
+/**
+ * beforeMain _ pribt statment before main to run
+ * Return:void
+ */
+void beforeMain(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }

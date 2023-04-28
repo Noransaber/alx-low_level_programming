aeafda611b72f5a00c13736251ad06e828c9dd35
Noransaber <noransaber685@gmail.com>
2023-04-28 00:49:45 -0700

    source code of free_node task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - delete the all nodes one by one/
+ * @head: pointer to the first elem in the list
+ *
+ * Return: voide
+ */
+
+
+void free_listint(listint_t *head)
+{
+struct listint_s *helper = head; /*init another pointer and it's equal head*/
+
+while (helper != NULL)/*loop for traversing over the list*/
+{
+helper = helper->next;/*helper move to the next node*/
+free(head);/*free head*/
+head = helper;/*head move to helper node*/
+}
+}

15b8dfc7c2e8f94bcae9341e0c99c7d6254c1047
Noransaber <noransaber685@gmail.com>
2023-04-29 09:39:41 -0700

    edit source code of pop the list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,30 +1,26 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
+ * pop_listint - removes the head node of the list
  * @head: ptr to the 1th elem in the linked list
  *
  * Return: the data in the ele that was deleted,
- * or 0 if the list is empty
+ * or return 0 if the list is empty
  */
-
-
 
 int pop_listint(listint_t **head)
 {
-int h_data;
-listint_t *h_link;
+listint_t *head_link;
+int head_data;
 
-if (!head || !*head)/*check if it empty*/
-{
+if (!head || !*head)
 return (0);
+
+head_data = (*head)->n;
+head_link = (*head)->next;
+free(*head);
+*head = head_link;
+
+return (head_data);
 }
 
-h_data = (*head)->n;
-h_link = (*head)->next;
-
-free(head);
-*head = h_link;
-
-return (h_data);
-}

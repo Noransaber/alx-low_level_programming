24d40e802dd79dbfdd7990914af709dbe6fe893c
Noransaber <noransaber685@gmail.com>
2023-04-29 04:11:48 -0700

    remove space for betty style

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - func that get node at given position
+ * @head: ptr to the first node of the list
+ * @index: given position
+ * Return: node at the given position
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i;
+struct listint_s *current = head;/*init ptr to head*/
+
+for (i = 0; i < index && current != NULL; i++)/*traversing throught the list*/
+{
+current = current->next;/*moving to the next node*/
+}
+if (current == NULL)
+{
+return (NULL);
+}
+return (current);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
+
 typedef struct listint_s
 {
     int n;

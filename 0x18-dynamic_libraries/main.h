d4a89c8d914780a816d5db2201e0e42aeb7cef12
Noransaber <noransaber685@gmail.com>
2023-06-19 03:10:06 -0700

    Updates

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 #include <unistd.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,3 +26,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */

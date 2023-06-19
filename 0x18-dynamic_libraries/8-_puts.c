9b66c2b5744cdeb38ae5a99a06a6b57282ad7a44
Noransaber <noransaber685@gmail.com>
2023-06-19 02:14:55 -0700

    Update

--- 0x18-dynamic_libraries/8-_puts.c
+++ 0x18-dynamic_libraries/8-_puts.c
@@ -1,6 +1,18 @@
-#include <unistd.h>
-
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
+/**
+ * _puts - put a string
+ * @s: string
+ * Return: NOTHING
+ **/
+
 void _puts(char *s)
 {
 	while (*s != '\0')
--- 0x18-dynamic_libraries/9-_strcpy.c
+++ 0x18-dynamic_libraries/9-_strcpy.c
@@ -23,5 +23,3 @@ char *_strcpy(char *dest, char *src)
 	dest[y] = '\0';
 	return (dest);
 }
- 
-

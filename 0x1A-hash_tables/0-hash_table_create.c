2134b3ca6a728b2bc850cc4a82f4a28dfc8a63e7
Noransaber <noransaber685@gmail.com>
2023-07-07 02:27:34 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *ht;
 unsigned long int i;
 
+if (size == 0)
+{
+return (NULL);
+}
 ht = malloc(sizeof(hash_table_t));
 if (ht == NULL)
 return (NULL);
@@ -19,7 +23,10 @@ return (NULL);
 ht->size = size;
 ht->array = malloc(sizeof(hash_node_t *) * size);
 if (ht->array == NULL)
+{
+free(ht);
 return (NULL);
+}
 for (i = 0; i < size; i++)
 ht->array[i] = NULL;
 

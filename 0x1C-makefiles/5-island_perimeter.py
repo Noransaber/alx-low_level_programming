f637a0cd3ae1e77174cfc1dce0892addb77a1976
Noransaber <noransaber685@gmail.com>
2023-08-23 18:34:52 +0300

    Updates pystylecode

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Island Perimeter"""
 
+
 def island_perimeter(grid):
-    """ returns the perimeter of
+    """returns the perimeter of
     the island described in grid"""
     width = len(grid[0])
     height = len(grid)
@@ -13,8 +14,8 @@ def island_perimeter(grid):
         for n in range(width):
             if grid[i][n] == 1:
                 size += 1
-                if (n > 0 and grid[i][n - 1] == 1):
+                if n > 0 and grid[i][n - 1] == 1:
                     edges += 1
-                if (i > 0 and grid[i - 1][n] == 1):
+                if i > 0 and grid[i - 1][n] == 1:
                     edges += 1
     return size * 4 - edges * 2

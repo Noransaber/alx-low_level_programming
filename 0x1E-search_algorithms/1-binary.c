cd06f1dc27409450e7195d9632a43d7b949130a8
Noransaber <noransaber685@gmail.com>
2023-10-09 22:18:31 +0300

    Source code for binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * binary_search -  searches for a value in a
+ * sorted arr of integers using the Binary search algorithm
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: key value we are searched
+ * Return: Always EXIT_SUCCESS
+ */
+
+ int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}

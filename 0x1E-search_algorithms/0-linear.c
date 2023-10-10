6dd8593437e524e2da69c7fc8c861b72880efd53
Noransaber <noransaber685@gmail.com>
2023-10-10 10:00:54 +0300

    Removing spaces

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
@@ -8,8 +7,6 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  **/
-
-
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
